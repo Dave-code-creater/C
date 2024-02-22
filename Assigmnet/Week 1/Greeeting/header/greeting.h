44fd0a3b46223a05cc2726e7ab5cea156a4804db
Dave <80132827+Dave-code-creater@users.noreply.github.com>
2024-02-22 18:59:00 +0000

    Add greet function and remove unused testing file

---
+++ Assigmnet/Week 1/Greeeting/header/greeting.h
@@ -0,0 +1,7 @@
+#ifndef GREETING_H
+#define GREETING_H
+
+// Function to print greeting message
+void greet(void);
+
+#endif
--- Assigmnet/Week 1/Greeeting/src/greeting.c
+++ Assigmnet/Week 1/Greeeting/src/greeting.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
+// Function prototype
+void greet(void);
+
+#ifdef GREETING_MAIN // Define GREETING_MAIN only when this file is compiled as a standalone program
 int main(void)
 {
-    // TODO
+    greet();
     return 0;
+}
+#endif
+// Function definition
+void greet(void)
+{
+    //TODO: Finish the function
+    printf("Hello, world!\n");
 }
